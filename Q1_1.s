f4a8be27b7fc4c1e5768fa36a55a1fbb8a3cd51a
karthikswarna <33567900+karthikswarna@users.noreply.github.com>
2019-03-15 17:18:32 +0530

    Update Q1_1.s

--- Q1_1.s
+++ Q1_1.s
@@ -2,7 +2,7 @@
 
 		.data
 var1:	.word	654858
-var2:	.word	455466  # (product = 4572032BA4)
+var2:	.word	455466 		# (product = 4572032BA4)
 
 		.text
 
@@ -10,7 +10,7 @@ main:
 	lw $t0, var1		# loading number in $t0.
 	lw $t1, var2		# loading number in $t1.
 	multu $t0, $t1		# $t0 * $t1.(stored in HI, LO)
-	mfhi $s0			# moving from HI to $s0.(=45)
-	mflo $s1			# moving from LO to $s1.(=72032BA4)
+	mfhi $s0		# moving from HI to $s0.(=45)
+	mflo $s1		# moving from LO to $s1.(=72032BA4)
 	jr $ra
 .end main
